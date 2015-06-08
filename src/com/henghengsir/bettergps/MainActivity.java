f56314cf94f523f6e0f875f1db0a9c67cd277de1
George <liusw@carit.com.cn>
2015-06-08 17:41:52 +0800

    add mock gps server and usb serial lib

--- src/com/henghengsir/bettergps/MainActivity.java
+++ src/com/henghengsir/bettergps/MainActivity.java
@@ -2,9 +2,10 @@
 package com.henghengsir.bettergps;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.Settings;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -13,4 +14,40 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkIfMockEnabled();
+    }
+    
+    private void checkIfMockEnabled() {
+        try {
+            int mock_location = Settings.Secure.getInt(getContentResolver(), "mock_location");
+            if (mock_location == 0) {
+                try {
+                    Settings.Secure.putInt(getContentResolver(), "mock_location", 1);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                mock_location = Settings.Secure.getInt(getContentResolver(), "mock_location");
+            }
+
+            if (mock_location == 0) {
+                showDialog(EnableMockLocationDialogFragment.MOCKDIALOG);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        // This is not done using a fragment directly as MapActivity does not
+        // allow fragments...
+        if (id == EnableMockLocationDialogFragment.MOCKDIALOG) {
+            return EnableMockLocationDialogFragment.createDialog(this);
+        }
+        return super.onCreateDialog(id);
+    };
 }
